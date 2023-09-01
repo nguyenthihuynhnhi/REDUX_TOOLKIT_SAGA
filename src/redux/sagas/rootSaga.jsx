12e0333146da150e0ee29ae9a992e5158ca19113
MACOS <macos@MacBook-Pro-cua-MACOS.local>
2023-09-01 14:02:05 +0700

    github

--- src/redux/sagas/rootSaga.jsx
+++ src/redux/sagas/rootSaga.jsx
@@ -2,6 +2,7 @@ import { all } from "redux-saga/effects";
 
 import * as userSaga from "./userSaga";
 
+
 // Tạo rootSaga kết hợp nhiều Saga Watcher lại với all()
 function* rootSaga() {
   // nếu call thêm API trong midware, thì tạo lúc export đặt tên theo cú pháp "follow[TênTypeDispatch]",
